826c51d4e6c62835d5c6984840fee81923fd0f5a
abekahFeynmann <abekaharthur5966@gmail.com>
2025-11-15 11:26:22 +0000

    added a new file and wrote a code for generating multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,7 @@
+#prompt user for a number.
+number = int(input("Enter a number to see its mulitiplication table: "))
+
+#Generate and print a mulitplication table.
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
