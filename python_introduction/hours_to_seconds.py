661aec0f775e39ab139685e874a61a0b9772adbb
abekahFeynmann <abekaharthur5966@gmail.com>
2025-11-15 16:47:45 +0000

    completed the task for this project

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+#definition of variables
+hours = 2
+
+#calculate the number of seconds in the hours
+seconds = 60 * 60 * hours
+
+#printing the result
+print(f"{hours} hour(s) is {seconds} seconds.")
